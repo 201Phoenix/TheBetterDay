6fbf38778c172394a21eb8976ba630889c4f51db
gintoki2000 <ngotrungaccor@gmail.com>
2020-07-09 14:35:38 +0700

    add DemoStory

---
+++ Assets/Testing/DemoTwine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoTwine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private TextAsset _textAsset;
+    void Start()
+    {
+        TwineStory story = TwineStory.Parse(_textAsset.text);
+        Debug.Log(story.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
