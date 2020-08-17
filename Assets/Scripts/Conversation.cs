4cc4db257c14516e8b12b975f0d18df495321e9c
gintoki2000 <ngotrungaccor@gmail.com>
2020-08-17 17:22:28 +0700

    xxx

---
+++ Assets/Scripts/Conversation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Conversation : ScriptableObject
+{
+	public string title;
+	[SerializeField]
+	public string[] sentences;	
+}
