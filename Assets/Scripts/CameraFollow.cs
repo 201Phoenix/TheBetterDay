20fac5334fb2edfaf2748982601165346149b053
Nguyen-Duc-Cong <45335633+Nguyen-Duc-Cong@users.noreply.github.com>
2020-08-18 00:53:56 +0700

    camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject[] player;
+    private Vector2 vectorPlayer;
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject vs in player)
+        {
+            vectorPlayer = new Vector2(vs.transform.position.x, vs.transform.position.y);
+        }
+        gameObject.transform.position = new Vector3(vectorPlayer.x,vectorPlayer.y,transform.position.z);
+    }
+}
